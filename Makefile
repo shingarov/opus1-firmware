264b2a3c4b672b91f3fd35c19fd4a0cf22e71bf2
Boris Shingarov <b1@shingarov.com>
2011-03-27 02:32:15 -0400

    Initial import

---
+++ Makefile
@@ -0,0 +1,36 @@
+#
+# Makefile for Ladarevo Opus.1 AVR firmware.
+# Copyright (c) 2010-2011 Ladarevo Software Inc.
+#
+
+CXX=avr-g++
+INCLUDES=-I/usr/share/arduino/hardware/arduino/cores/arduino
+CXXFLAGS=$(INCLUDES) -g -Os -w -fno-exceptions -ffunction-sections -fdata-sections -mmcu=atmega2560 -DF_CPU=16000000L -DARDUINO=22
+
+OBJS=opus1.o PS2Keyboard.o
+
+DUDE=/usr/share/arduino/hardware/tools/avrdude
+DUDECONF=/usr/share/arduino/hardware/tools/avrdude.conf
+DUDEBUG=-v -v -v -v
+DUDEFLAGS=-C$(DUDECONF) $(DUDEBUG) -patmega2560 -cstk500v2 -P/dev/ttyACM0 -b115200 -D 
+
+
+all: opus1.hex opus1.eep
+
+opus1.elf: $(OBJS) core.a
+	avr-gcc -Os -Wl,--gc-sections -mmcu=atmega2560 -o opus1.elf $(OBJS) core.a -lm
+
+opus1.eep: opus1.elf
+	avr-objcopy -O ihex -j .eeprom --set-section-flags=.eeprom=alloc,load --no-change-warnings --change-section-lma .eeprom=0 opus1.elf opus1.eep
+
+opus1.hex: opus1.elf
+	avr-objcopy -O ihex -R .eeprom opus1.elf opus1.hex 
+
+upload:
+	$(DUDE) $(DUDEFLAGS) -Uflash:w:opus1.hex:i
+
+
+
+clean:
+	rm -f *.o *.elf *.hex *.eep
+
---
+++ opus1.cpp
@@ -0,0 +1,53 @@
+#include "PS2Keyboard.h"
+#include "WProgram.h"
+
+void setup();
+void loop();
+const int DataPin = 20;
+const int IRQpin =  21;
+
+PS2Keyboard keyboard;
+
+void setup() {
+  delay(1000);
+  keyboard.begin(DataPin, IRQpin);
+  Serial.begin(9600);
+  Serial.println("Keyboard Test:");
+}
+
+void loop() {
+  if (keyboard.available()) {
+    
+    // read the next key
+    char c = keyboard.read();
+    
+    // check for some of the special keys
+    if (c == PS2_ENTER) {
+      Serial.println();
+    } else if (c == PS2_TAB) {
+      Serial.print("[Tab]");
+    } else if (c == PS2_ESC) {
+      Serial.print("[ESC]");
+    } else if (c == PS2_PAGEDOWN) {
+      Serial.print("[PgDn]");
+    } else if (c == PS2_PAGEUP) {
+      Serial.print("[PgUp]");
+    } else if (c == PS2_LEFTARROW) {
+      Serial.print("[Left]");
+    } else if (c == PS2_RIGHTARROW) {
+      Serial.print("[Right]");
+    } else if (c == PS2_UPARROW) {
+      Serial.print("[Up]");
+    } else if (c == PS2_DOWNARROW) {
+      Serial.print("[Down]");
+    } else if (c == PS2_DELETE) {
+      Serial.print("[Del]");
+    } else {
+      
+      // otherwise, just print all normal characters
+      Serial.print(c);
+    }
+  }
+}
+
+
