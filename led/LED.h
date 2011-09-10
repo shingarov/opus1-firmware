1430d0a11113892fabf4b5274f6844482b894d22
Boris Shingarov <b1@shingarov.com>
2011-09-10 05:55:06 -0400

    Copyright notices

--- led/LED.h
+++ led/LED.h
@@ -5,6 +5,19 @@
  *   setupLED();
  *   ledON(int x, int y);  0<=x<=7, 0<=y<=7;
  *   ledOFF(int x, int y);  0<=x<=7, 0<=y<=7;
+ *
+ * OPUS.1 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OPUS.1 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OPUS.1.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 void setupLED();
--- led/opus1.cpp
+++ led/opus1.cpp
@@ -1,3 +1,23 @@
+/** 
+ * Ladarevo Opus.1 LED firmware, main program.
+ *
+ * Copyright (c) 2009-2011 LADAREVO SOFTWARE INC.
+ *
+ * OPUS.1 is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * OPUS.1 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OPUS.1.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
 #include "WProgram.h"
 #include "LED.h"
 #include "OK.h"
